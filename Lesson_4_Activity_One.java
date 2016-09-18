e61e1a0967e4a77d3aff80ae1101cbc24d740bb2
Seunghyun Kim <qwazqwert@gmail.com>
2016-09-18 16:57:17 -0400

    Update Lesson_4_Activity_One.java

--- Lesson_4_Activity_One.java
+++ Lesson_4_Activity_One.java
@@ -1,3 +1,11 @@
+/*Input two doubles and print them backwards on the screen.
+
+  So if the user enters:
+1.3 6.8
+  It should output:
+6.8 1.3
+  Notice there is exactly one space between the numbers. */
+
 import java.util.Scanner;
 
 class Lesson_4_Activity_One {
