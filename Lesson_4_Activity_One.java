81b68ebec8935b7c985c99df46d79913668cfb6b
Seunghyun-kim <qwazqwert@gmail.com>
2016-09-18 15:32:37 -0400

    Uploaded Lesson 4 Activity One

---
+++ Lesson_4_Activity_One.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Lesson_4_Activity_One {
+    public static void main(String[] args) {
+      
+		Scanner input = new Scanner (System.in);  // create input read object
+		double x = input.nextDouble();	// read the first input
+		double y = input.nextDouble();	// read the second input
+		System.out.print(y + " " + x);	// print output
+
+	}
+
+}
